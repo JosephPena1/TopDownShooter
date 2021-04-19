2b3e9bf1947b6e458cb54ac19f8720147816354e
Joseph Peña <jgpena2888@gmail.com>
2021-04-19 16:46:28 -0500

    Made Turret Prefab

---
+++ Assets/Scripts/Gameplay/TurretShootBehaviour.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretShootBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _target;
+    [SerializeField]
+    private BulletEmitterBehaviour _bulletEmitter;
+    [SerializeField]
+    private float _shotSpeed = 1;
+    [SerializeField]
+    private float _range = 10;
+
+    private float _coolDown = 100;
+
+    private Vector3 _direction;
+
+    public GameObject Target
+    {
+        get { return _target; }
+        set { _target = value; }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Gets the distance from the turret to the target
+        float targetDistance = (_target.transform.position - transform.position).magnitude;
+
+        //if the gameobject has an [Player] tag and target is in range, lookat target
+        if (_target.CompareTag("Player") && targetDistance < _range && _bulletEmitter)
+        {
+            transform.LookAt(_target.transform);
+
+            if (_coolDown <= 0)
+            {
+                _bulletEmitter.Fire(transform.forward * _shotSpeed);
+                _coolDown = 100;
+            }
+        }
+
+        if (_coolDown < 0)
+            _coolDown = 0;
+        else
+            _coolDown--;
+    }
+}
---
+++ Assets/Scripts/Gameplay/TurretSpawnerBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretSpawnerBehaviour : MonoBehaviour
+{
+    [Tooltip("The object that will be instantiated.")]
+    [SerializeField]
+    private GameObject _spawnObject;
+    [Tooltip("The amount of time in seconds between each spawn.")]
+    [SerializeField]
+    private float _timeBetweenSpawns;
+    [Tooltip("If false, the spawner will stop instantiating clones of the reference.")]
+    [SerializeField]
+    private bool _canSpawn;
+    [Tooltip("The enemy object's target.")]
+    [SerializeField]
+    private GameObject _enemyTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnObjects());
+    }
+
+    /// <summary>
+    /// Spawns objects continuously while canSpawn is true.
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerator SpawnObjects()
+    {
+        while (_canSpawn)
+        {
+            //Creates a vector3 with a random x and z value;
+            Vector3 randomPos = new Vector3(Random.Range(-50, 50), 0, Random.Range(-50, 50));
+            //Create a new enemy in the scene
+            GameObject spawnedEnemy = Instantiate(_spawnObject, randomPos, new Quaternion());
+            //Set the enemy target to be the target the spawner was given
+            spawnedEnemy.GetComponent<TurretShootBehaviour>().Target = _enemyTarget;
+            //Pause for the given time in seconds before resuming the function
+            yield return new WaitForSeconds(_timeBetweenSpawns);
+        }
+    }
+}
